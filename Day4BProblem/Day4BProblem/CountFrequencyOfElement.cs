e062850d0a7839215798249e6d19376effd4df7b
“codewitherrors” <codewitherrors1@gmail.com>
2023-07-09 08:58:17 +0530

    {Sagar} Add : Count the frequecy of element

---
+++ Day4BProblem/Day4BProblem/CountFrequencyOfElement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4BProblem
+{
+    public class CountFrequencyOfElement
+    {
+        public void FrequencyCount()
+        {
+            //Array Declaration
+            int[] arr = { 1, 2, 4, 5, 1, 2, 3 };
+            int length = arr.Length;
+            int i, j;
+            i = j = 0;
+            int count;
+
+            //Outer Loop
+            for (i = 0; i < length; i++)
+            {
+                int x = arr[i];
+                count = 0;
+                if (x == -1) { continue; }
+                for (j = 0; j < length; j++)
+                {
+                    if (arr[j] == x)
+                    {
+                        count++;
+                        arr[j] = -1;
+                    }
+                }
+                Console.WriteLine($"The Frequency of {x} is {count}");
+            }
+        }
+    }
+}
---
+++ Day4BProblem/Day4BProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Day4BProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            CountFrequencyOfElement c1 = new CountFrequencyOfElement();
+            c1.FrequencyCount();
+        }
+
+    }
+}
